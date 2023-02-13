d7e568f580458ca6424bc2d2663de8f2d4339a71
Сергей Усачев <usachevplayer@mail.ru>
2023-02-13 18:42:51 +0200

    c

--- BomjiDeruca/Program.cs
+++ BomjiDeruca/Program.cs
@@ -9,62 +9,64 @@
 //при получении урона немного себя лечит.Будут новые поля у наследников.
 //У кого-то может быть мана и это только его особенность.
 
-namespace BomjiDeruca
+using System;
+
+abstract class Fighter
 {
-	internal class Program
+	public string Name { get; }
+	public int HP { get; set; }
+	public int Attack { get; }
+	public int Defense { get; }
+
+	public Fighter(string name, int hp, int attack, int defense)
 	{
-		static void Main(string[] args)
-		{
-			
-		}
+		Name = name;
+		HP = hp;
+		Attack = attack;
+		Defense = defense;
 	}
 
-	abstract class Fighter
+	public abstract void SpecialAttack(Fighter other);
+}
+
+class Knight : Fighter
+{
+	public Knight(string name, int hp, int attack, int defense) : base(name, hp, attack, defense)
 	{
-		protected string _name;
-		protected int _health;
-		protected int _maxHealth;
-		protected int _damage;
-		protected int _mana;
-		protected int _maxMana;
-		protected int _armor;
-
-		public Fighter(string name, int maxHealth = 100, int damage = 10, int maxMana = 0, int armor = 0)
-		{
-			_name=name;
-			_maxHealth=maxHealth;
-			_health = maxHealth;
-			_damage=damage;
-			_maxMana=maxMana;
-			_mana = maxMana;
-			_armor=armor;
-		}
-
-		public void ShowStats()
-		{
-			Console.WriteLine($"Имя: {_name}, Здоровье: {_maxHealth}, Урон: {_damage}," +
-				$" Мана: {_mana}, Защита: {_armor} ");
-		}
-
-		abstract public void ShowInfo();
-
-		abstract public void DoHit();
-
-		abstract public void TakeHit();
-
-		abstract public void Skill();
 	}
 
-
-	class Warrior : Fighter
+	public override void SpecialAttack(Fighter other)
 	{
-		public Warrior(string name)
-		{
-			name = this._name;
-		}
-		public override void ShowInfo()
-		{
-			Console.WriteLine($"{GetType().Name}, {_name}");
-		}
+		int damage = Attack * 2 - other.Defense;
+		other.HP -= damage;
+		Console.WriteLine($"{Name} uses Double Strike and deals {damage} damage to {other.Name}.");
+	}
+}
+
+class Archer : Fighter
+{
+	public Archer(string name, int hp, int attack, int defense) : base(name, hp, attack, defense)
+	{
+	}
+
+	public override void SpecialAttack(Fighter other)
+	{
+		int damage = Attack + 3;
+		other.HP -= damage;
+		Console.WriteLine($"{Name} uses Poison Shot and deals {damage} damage to {other.Name}.");
+	}
+}
+
+class Mage : Fighter
+{
+	public Mage(string name, int hp, int attack, int defense) : base(name, hp, attack, defense)
+	{
+	}
+
+	public override void SpecialAttack(Fighter other)
+	{
+		int damage = Attack * 3 - other.Defense;
+		other.HP -= damage;
+		Console.WriteLine($"{Name} uses Fireball and deals {damage} damage to {other.Name}.");
 	}
 }
